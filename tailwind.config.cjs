442041e4c356076a0fe0beab174f6ea02dc921dc
Kayh <basedlainpilled@gmail.com>
2023-08-09 17:07:23 -0400

    init website

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        marquee: "marquee 45s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-400%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
