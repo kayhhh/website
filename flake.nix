30ca7c494fc8a8ee24421a093b0cb89d4755e88d
Kayh <basedlainpilled@gmail.com>
2023-12-30 19:56:18 -0500

    set up leptos

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
 
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs;
-            [ cargo-watch rust-analyzer rustBin ] ++ build_inputs;
+            [ cargo-watch leptosfmt rust-analyzer rustBin ] ++ build_inputs;
           nativeBuildInputs = native_build_inputs;
 
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath build_inputs;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
-pub fn hello() {
-    println!("Hello, world!");
+use leptos::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    mount_to_body(|| {
+        view! { <App /> }
+    })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                WriteSignal::set(&set_count, ReadSignal::get(&count) + 1);
+            }
+        >
+            "Click me: "
+            {move || ReadSignal::get(&count)}
+        </button>
+    }
 }
