c5aa157f430e01dbda24bfb7e5bc41ab5f9b2ff9
Kayh <basedlainpilled@gmail.com>
2023-12-30 21:29:01 -0500

    card layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use leptos::*;
 use wasm_bindgen::prelude::*;
 
+mod components;
+
+use components::*;
+
 #[wasm_bindgen(start)]
 pub fn start() {
     mount_to_body(|| {
@@ -11,10 +15,39 @@ pub fn start() {
 #[component]
 fn App() -> impl IntoView {
     view! {
-        <div class="p-8 space-y-4">
-            <h1>kayh.dev</h1>
+        <div class="max-w-2xl mx-auto py-8 grid gap-4">
+            <h1 class="text-center">"kayh.dev"</h1>
 
-            <Links/>
+            <div class="grid gap-4">
+                <span class="col-span-2">
+                    <Card>
+                        <About/>
+                    </Card>
+                </span>
+
+                <Card>
+                    <HotlineWebring/>
+                </Card>
+
+                <span class="row-span-2">
+                    <Card>
+                        <Links/>
+                    </Card>
+                </span>
+            </div>
+        </div>
+    }
+}
+
+#[component]
+fn About() -> impl IntoView {
+    view! {
+        <div>
+            <h2>"About"</h2>
+
+            <p>
+                "Hi, I'm Kayh! Welcome to my site! I'm a self-taught developer building things I think are cool. I play a lot of VRChat, so if you see me around come say hi!"
+            </p>
         </div>
     }
 }
@@ -23,30 +56,41 @@ fn App() -> impl IntoView {
 fn Links() -> impl IntoView {
     view! {
         <div>
-            <h2>Links</h2>
+            <h2>"Links"</h2>
 
             <ul>
                 <li>
                     <a href="https://github.com/kayhhh" target="_blank">
-                        "GitHub"
+                        "[GitHub]"
                     </a>
                 </li>
                 <li>
                     <a href="https://twitter.com/kayh_online" target="_blank">
-                        "Twitter"
+                        "[Twitter]"
                     </a>
                 </li>
                 <li>
                     <a href="https://mirror.xyz/kayh-online.eth" target="_blank">
-                        "Blog"
+                        "[Blog]"
                     </a>
                 </li>
                 <li>
                     <a href="https://myanimelist.net/profile/kayh_online" target="_blank">
-                        "MyAnimeList"
+                        "[MyAnimeList]"
                     </a>
                 </li>
             </ul>
+        </div>
+    }
+}
+
+#[component]
+fn HotlineWebring() -> impl IntoView {
+    view! {
+        <div class="flex justify-between space-x-4">
+            <a href="https://hotlinewebring.club/kayh/previous">"<---"</a>
+            <p class="text-amber-300">Hotline Webring</p>
+            <a href="https://hotlinewebring.club/kayh/next">"--->"</a>
         </div>
     }
 }
